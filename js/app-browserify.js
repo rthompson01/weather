b79d3693747dc5cf28909a06aa24d3e3265a9cd9
Rey Thompson <rthompson1@gmail.com>
2015-06-04 09:46:43 -0500

    second commit

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -23,4 +23,34 @@ var Promise = require('es6-promise').Promise
     // start app
     // new Router()
 // }
+var $ = require('jquery')
+var key = '05f8f85305996bad2f4729ca8dc52c1d'
 
+var GPS = new Promise((res, rej) => {
+    // if gps successful, resolve with coordinates
+    // else reject with error
+    navigator.geolocation.getCurrentPosition(
+        (gpsData) => res({ lat: gpsData.coords.latitude, lon: gpsData.coords.longitude }),
+        (error) => rej(error.message)
+    )
+})
+
+GPS.then((ll) => {
+    var url = `https://api.forecast.io/forecast/${key}/${ll.lat},${ll.lon}?callback=?`
+    var x = $.getJSON(url).then((r) => console.log(r))
+})
+
+require("ti.forecast.io");
+var forecast = new ForecastIO({
+    API_KEY: 'API_KEY'
+});
+
+var condition = forecast.getCurrentConditions(lat, lon, onSuccessCallback, onErrorCallback);
+
+function onSuccessCallback(weather) {
+  weather.getSummary();
+};
+
+function onErrorCallback(e) {
+  // Handle your errors in here
+};
